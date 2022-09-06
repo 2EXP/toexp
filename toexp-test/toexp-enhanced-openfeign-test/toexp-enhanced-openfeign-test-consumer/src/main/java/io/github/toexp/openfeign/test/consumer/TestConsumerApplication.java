9dec4d922f211bb5d3ec27f2778d8cbe3aa7adef
2EXP <26007713+2EXP@users.noreply.github.com>
2022-09-06 08:43:28 +0800

    add spring.factories

--- toexp-test/toexp-enhanced-openfeign-test/toexp-enhanced-openfeign-test-consumer/src/main/java/io/github/toexp/openfeign/test/consumer/TestConsumerApplication.java
+++ toexp-test/toexp-enhanced-openfeign-test/toexp-enhanced-openfeign-test-consumer/src/main/java/io/github/toexp/openfeign/test/consumer/TestConsumerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(scanBasePackages = {"io.github.toexp.openfeign"})
-@EnableFeignClients(basePackages = {"io.github.toexp.openfeign.test"})
+@SpringBootApplication
+@EnableFeignClients(basePackages = {"io.github.toexp.openfeign.test.api"})
 public class TestConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestConsumerApplication.class, args);
--- toexp-test/toexp-enhanced-openfeign-test/toexp-enhanced-openfeign-test-provider/src/main/java/io/github/toexp/openfeign/test/provider/TestProviderApplication.java
+++ toexp-test/toexp-enhanced-openfeign-test/toexp-enhanced-openfeign-test-provider/src/main/java/io/github/toexp/openfeign/test/provider/TestProviderApplication.java
@@ -3,7 +3,7 @@ package io.github.toexp.openfeign.test.provider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"io.github.toexp.openfeign.test.provider"})
+@SpringBootApplication
 public class TestProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestProviderApplication.class, args);
